e47b8cb9a47c7cb046cf50015cd118b775a34082
pschengel <136333034+pschengel@users.noreply.github.com>
2023-08-16 16:06:24 +0200

    helper

--- resources/sendFeedback.groovy
+++
@@ -1,9 +0,0 @@
-
-def response = httpRequest(
-    url: 'https://eohotv9zrfaegyf.m.pipedream.net',
-    contentType: 'APPLICATION_JSON',
-    httpMode: 'POST',
-    requestBody: "{\"result\": \"${currentBuild.result}\", \"message\": \"Your custom message here\"}"
-)
-echo "Feedback response: ${response}"
- 
